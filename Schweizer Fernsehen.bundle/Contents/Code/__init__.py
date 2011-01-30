be2e0fea7bc23db128374aefcbccc2c2385d21b0
Christian Hitz <christian@klarinett.li>
2011-01-30 13:59:25 +0100

    remove search

--- Schweizer Fernsehen.bundle/Contents/Code/__init__.py
+++ Schweizer Fernsehen.bundle/Contents/Code/__init__.py
@@ -12,7 +12,7 @@ CACHE_INTERVAL = 3600
 
 ####################################################################################################
 def Start():
-    Plugin.AddPrefixHandler(SF_PREFIX, MainMenu, 'Schweizer Fernsehen', 'icon-default.jpg', 'art-default.jpg')
+    Plugin.AddPrefixHandler(SF_PREFIX, GetShowOverview, 'Schweizer Fernsehen', 'icon-default.jpg', 'art-default.jpg')
     Plugin.AddViewGroup("Details", viewMode="InfoList", mediaType="items")
     MediaContainer.title1 = 'Schweizer Fernsehen'
     MediaContainer.art = R('art-default.jpg')
@@ -24,28 +24,8 @@ def UpdateCache():
     HTTP.Request(SF_CHANNELS)
 
 ####################################################################################################
-def MainMenu():
-    dir = MediaContainer()
-    dir.Append(Function(DirectoryItem(GetShowOverview,   title=L("Shows"), thumb=R('icon-default.jpg'))))
-    dir.Append(Function(SearchDirectoryItem(Search,       title=L("Search"), prompt=L("Search for Episodes"), thumb=R('search.png'))))
-    return dir
-
-####################################################################################################
-def GetChannelsMenu(sender):
-    dir = MediaContainer(viewGroup='Details', title2=sender.itemTitle)
-    xml = HTML.ElementFromURL(SF_CHANNELS)
-    for show in xml.xpath('//div[@class="channel"]'):
-        url = SF_ROOT + show.xpath('div[@class="keyvisual"]/a')[0].get('href')
-        title = show.xpath('div[@class="keyvisual"]/a/img')[0].get('alt')
-        description = show.xpath('div[@class="channel_info"]/p')[0].text
-        try: thumb = SF_ROOT + show.xpath('div[@class="keyvisual"]/a/img')[0].get('src')
-        except: thumb = None
-        dir.Append(WebVideoItem(url, title=title, thumb=thumb, summary=description))
-    return dir
-
-####################################################################################################
-def GetShowOverview(sender):
-    dir = MediaContainer(viewGroup='Details', title2=sender.itemTitle)
+def GetShowOverview():
+    dir = MediaContainer(viewGroup='Details')
     xml = HTML.ElementFromURL(SF_SHOWS)
     for show in xml.xpath('//div[@class="az_row"]'):
         key = SF_ROOT + show.find('a').get('href')
@@ -143,68 +123,3 @@ def GetPreviousEpisodes(sender, url, showTitle, previousEpisode=False):
         dir.Extend(GetPreviousEpisodes(sender, url, showTitle))
 
     return dir
-
-####################################################################################################
-def Search(sender, query, page=None, start_video=0):
-    dir = MediaContainer(viewGroup='Details', title2=query)
-
-    search_url = SF_SEARCH + '?query=' + query
-    if (page):
-        search_url += "&page=" + str(page)
-
-    xml = HTML.ElementFromURL(search_url)
-    #shows
-    if start_video == 0:
-        try:
-            shows = xml.xpath('//div[@id="search_result_sendungen"]')[0]
-            for sendung in shows.xpath('div/div[@class="reiter_item"]'):
-                url = SF_ROOT + sendung.xpath('h3/a[@class="title"]')[0].get('href')
-                title = sendung.xpath('h3/a[@class="title"]/strong')[0].text
-                try: thumb = sendung.xpath('h3/a[@class="sendung_img_wrapper"]/img')[0].get('src')
-                except: thumb = None
-                dir.Append(Function(DirectoryItem(GetEpisodeMenu, title=title, thumb=thumb, url=url), url=url))
-        except:
-            pass
-
-    #Videos
-    number_shows = 0;
-    try:
-        shows = xml.xpath('//div[@id="search_result_videos"]')[0]
-        for show in shows.xpath('div[@class="result_video_row"]')[start_video:]:
-            video_url = SF_ROOT + show.xpath('div/h3/a[@class="video_title"]')[0].get('href').split(';')[0]
-
-            try: thumb = show.xpath('a[@class="sendung_img_wrapper"]/img')[0].get('src')
-            except: thumb = None
-            try: title = show.xpath('div/h3/a[@class="video_title"]')[0].text
-            except: title = None
-            try: summary = show.xpath('div/p[@class="video_description"]')[0].text
-            except: summary = None
-            dir.Append(WebVideoItem(video_url, title=title, thumb=thumb, summary=summary))
-
-            number_shows += 1
-            if number_shows == 9:
-                dir.Append(Function(DirectoryItem(Search, title=L("More Results"), url=SF_SEARCH), query=query, page=page, start_video=(start_video + number_shows)))
-                return dir
-    except:
-        pass
-    try:
-        current_page = int(xml.xpath('//div[@id="skim_results"]/a[@class="act_site"]')[0].text)
-        max_page = 1
-        try:
-            for page in xml.xpath('//div[@id="skim_results"]/a'):
-                if (page.get('href')):
-                    page_nr = int(page.get('href').rsplit('=',1)[1])
-                    if (page_nr > max_page):
-                        max_page = page_nr
-        except:
-            pass
-
-        if (current_page < max_page):
-            dir.Append(Function(DirectoryItem(Search, title=L("More Results"), url=SF_SEARCH), query=query, page=(current_page + 1)))
-            return dir
-    except:
-        #no additional pages
-        pass
-
-    return dir
-
